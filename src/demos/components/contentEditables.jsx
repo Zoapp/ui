b4589fd5d9b0242b70c978e5c33417a773f0f972
mikbry <mik@opla.ai>
2018-02-18 12:57:35 +0100

    Refactor scripts, code for demos, css

---
+++ src/demos/components/contentEditables.jsx
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2015-present, CWB SAS
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from "react";
+import {
+  ContentEditable,
+} from "../../../src/libs";
+
+export default () => (
+  <section>
+    <h1>ContentEditable examples </h1>
+    <div style={{ padding: "16px" }}>
+      <ContentEditable />
+    </div>
+    <h2>Tabs Properties</h2>
+  </section>
+);
---
+++ src/demos/main.jsx
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2015-present, CWB SAS
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from "react";
+import PropTypes from "prop-types";
+import ContentEditables from "./components/contentEditables";
+import ExpansionPanels from "./components/expansionPanels";
+import Steppers from "./components/steppers";
+
+const Main = ({ children }) => (
+  <main>
+    {children}
+    <ContentEditables />
+    <ExpansionPanels />
+    <Steppers />
+    <section style={{ height: "200px" }} />
+  </main>
+);
+
+Main.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Main;

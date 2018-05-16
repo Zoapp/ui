b1cb445e96f93a47bc7d56644bd56ce2a2577ca3
lodelestra <loicdelestra@hotmail.fr>
2018-05-16 18:38:16 +0200

    add DrawerFooter

--- src/libs/index.js
+++ src/libs/index.js
@@ -17,6 +17,7 @@ import ExpansionPanel from "./components/expansionPanel";
 import TableComponent from "./components/tableComponent";
 import Tooltip from "./components/tooltip";
 import FileInput from "./components/fileInput";
+import DrawerFooter from "./components/drawerFooter";
 
 export {
   Stepper,
@@ -32,4 +33,5 @@ export {
   TableComponent,
   Tooltip,
   FileInput,
+  DrawerFooter,
 };

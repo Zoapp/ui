5893b8801150b2560ecd4a46075e90d0360c62c5
Arnaud MONCEL <arnaud.moncel43@gmail.com>
2019-01-07 15:19:08 +0100

    Update webpack-dev-server to fix vulnerability
    Copy-webpack-plugin: 4.0.6
    File-loader: 3.0.1
    Webpack: ^4.28.3
    Webpack-cli: ^3.2.0
    Webpack-dev-server: ^3.1.14
    Webpack-merge: ^4.2.1

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,7 @@
+/* eslint 
+  import/no-extraneous-dependencies: 0,
+  no-undef: 0
+*/
 const path = require("path");
 const webpack = require("webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
@@ -5,21 +9,21 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   context: path.resolve(__dirname, "./src/"),
   entry: {
-    app: "./demos/index.jsx"
+    app: "./demos/index.jsx",
   },
   output: {
     path: path.resolve(__dirname, "./dist/public/js"),
     filename: "app.js",
     publicPath: "/",
     hotUpdateChunkFilename: "hot/hot-update.js",
-    hotUpdateMainFilename: "hot/hot-update.json"
+    hotUpdateMainFilename: "hot/hot-update.json",
   },
   resolve: {
     extensions: [".js", ".jsx"],
     alias: {
       RMComponents: path.resolve(__dirname, "./src/libs"),
     },
-    modules: [path.join(__dirname, "src"), "node_modules"]
+    modules: [path.join(__dirname, "src"), "node_modules"],
   },
   module: {
     rules: [
@@ -28,36 +32,36 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      '__REACT_DEVTOOLS_GLOBAL_HOOK__': '({ isDisabled: true })'
+      __REACT_DEVTOOLS_GLOBAL_HOOK__: "({ isDisabled: true })",
     }),
     new CopyWebpackPlugin([
       {
         from: "./public",
         to: path.resolve(__dirname, "./dist/public"),
-        force: true
+        force: true,
       },
       {
         from: "../dist/compressed.css",
         to: path.resolve(__dirname, "./dist/public/css/compressed.css"),
-        force: true
+        force: true,
       },
       {
         from: "./server",
         to: path.resolve(__dirname, "./dist"),
-        force: true
+        force: true,
       },
       {
         from: path.resolve(__dirname, "./package.json"),
         to: path.resolve(__dirname, "./dist/package.json"),
-        force: true
-      }
-    ])
-  ]
+        force: true,
+      },
+    ]),
+  ],
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,38 +1,40 @@
+/* eslint import/no-extraneous-dependencies: 0 */
 const merge = require("webpack-merge");
 const commonConfig = require("./webpack.common.js");
 const path = require("path");
-const webpack = require("webpack");
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+// eslint-disable-next-line no-undef
 module.exports = merge(commonConfig, {
+  mode: "development",
   entry: [
     "react-hot-loader/patch",
     "webpack-dev-server/client?http://localhost:3033",
     "webpack/hot/only-dev-server",
-    "./demos/index.jsx"
+    "./demos/index.jsx",
   ],
   devtool: "inline-source-map",
   devServer: {
     hot: true,
-    contentBase: path.resolve(__dirname, "./dist/public"),
+    contentBase: path.resolve(__dirname, "./dist/public"), // eslint-disable-line no-undef
     port: 3033,
     host: "localhost",
     publicPath: "/",
     historyApiFallback: true,
   },
   module: {
-    loaders: [
-      { test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|svg|woff2)$/, loader: "file?name=[name].[ext]" }
-    ],
     rules: [
       {
+        test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|svg|woff2)$/,
+        loader: "file?name=[name].[ext]",
+      },
+      {
         test: /\.jsx?$/,
-        use: ["babel-loader",],
-        exclude: /node_modules/
+        use: ["babel-loader"],
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader?modules",],
+        use: ["style-loader", "css-loader?modules"],
       },
       {
         test: /\.(ttf|eot|woff|woff2)$/,
@@ -42,9 +44,5 @@ module.exports = merge(commonConfig, {
         },
       },
     ],
-  },  
-  plugins: [
-    /*new webpack.HotModuleReplacementPlugin(),*/
-    new webpack.NamedModulesPlugin(),
-  ]
+  },
 });
